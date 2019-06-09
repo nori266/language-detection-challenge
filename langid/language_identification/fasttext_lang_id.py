fdbb07f51e389d9bdd88bc5b2470be2b64be1ae2
nori266 <noriel266@gmail.com>
2019-06-09 13:19:56 +0700

    implemented language identifiation

--- langid/language_identification/fasttext_lang_id.py
+++ langid/language_identification/fasttext_lang_id.py
@@ -1,7 +1,9 @@
 import os
 from pathlib import Path
+import pickle
 from typing import Iterable
 from typing import List
+from typing import Union
 
 from pyfasttext import FastText
 
@@ -19,9 +21,12 @@ class FasttextLangId:
 
     __DIRNAME = Path(os.path.dirname(__file__))
     __OUTPUT_TRAINED_MODEL = __DIRNAME / 'models/dialect_clf_model'
+    __NO_PREDICTION_VALUE = 'None'
+    __CLASSES = ['ptpt', 'ptbr']
 
     def __init__(self, path_to_model: str):
         self.__model = FastText(path_to_model)
+        self.classes = self.__model.labels
 
     def fit(self, filename: str, epoch: int = 100) -> 'FasttextLangId':
         """Trains model given filename - path to train data file.
@@ -32,6 +37,7 @@ class FasttextLangId:
         """
 
         self.__model.supervised(input=filename, output=self.__OUTPUT_TRAINED_MODEL, epoch=epoch, lr=0.7)
+        self.classes = self.__model.labels
         return self
 
     def predict(self, texts: Iterable[str]) -> List[str]:
@@ -40,7 +46,9 @@ class FasttextLangId:
         :param texts: input texts
         :return: list of class values - language codes
         """
-        pass
+
+        predictions = self.__model.predict(texts)
+        return [pred[0] if pred else self.__NO_PREDICTION_VALUE for pred in predictions]
 
     def predict_file(self, filename: str) -> List[str]:
         """Assigns a class value for each text in texts from file.
@@ -48,21 +56,34 @@ class FasttextLangId:
         :param filename: file with texts for input
         :return: list of class values - language codes
         """
-        pass
 
-    def evaluate(self, filename: str) -> None:
-        """Evaluates precision and recall metrics for each class.
+        predictions = self.__model.predict_file(filename)
+        return [pred[0] if pred else self.__NO_PREDICTION_VALUE for pred in predictions]
 
-        :param filename: file with test set of texts
+    def evaluate(self, texts: Union[Iterable[str], str], gold_labels: Iterable[str],
+    ) -> None:
+        """Evaluates precision and recall metrics for each class and prints them.
+
+        :param texts: iterable of texts or file with test set of texts
+        :param gold_labels: ground truth
         """
-        pass
+
+        if isinstance(texts, Iterable):
+            predictions = self.predict(texts)
+        else:
+            predictions = self.predict_file(texts)
+
+        for class_label in self.classes:
+            print(f"Precision for class {class_label}: {precision(gold_labels, predictions, class_label)}")
+            print(f"Recall for class {class_label}: {recall(gold_labels, predictions, class_label)}")
 
     def save(self, filename: str) -> None:
         """Saves model to file.
 
         :param filename: file to save to
         """
-        pass
+
+        pickle.dump(self, (filename))
 
     @classmethod
     def load(cls, filename: str) -> 'FasttextLangId':
@@ -71,4 +92,38 @@ class FasttextLangId:
         :param filename: file to load from
         :return: model -s class instance
         """
-        return model
+        return pickle.load(filename)
+
+
+def precision(true_values: Iterable, predictions: Iterable, positive_class: str) -> float:
+    """Calculates precision metric for positive_class. Comparing to sklearn precision_score
+    this function set values to False if they match neither positive nor negative class.
+
+    :param true_values:
+    :param predictions:
+    :param positive_class: the label of class considered positive for the metric: must be 'ptpt' or 'ptbr'
+    :return: precision score
+    """
+
+    true_positive = len([pred for (pred, gold) in zip(predictions, true_values)
+                         if (pred == gold and pred == positive_class)])
+    positive = len([pred for pred in predictions if pred == positive_class])
+
+    return true_positive / positive
+
+
+def recall(true_values: Iterable, predictions: Iterable, positive_class: str) -> float:
+    """Calculates recall metric for positive_class. Comparing to sklearn precision_score
+    this function set values to False if they match neither positive nor negative class.
+
+    :param true_values:
+    :param predictions:
+    :param positive_class: the label of class considered positive for the metric: must be 'ptpt' or 'ptbr'
+    :return: recall score
+    """
+
+    true_positive = len([pred for (pred, gold) in zip(predictions, true_values)
+                         if (pred == gold and pred == positive_class)])
+    all_true = len([value for value in true_values if value == positive_class])
+
+    return true_positive / all_true
