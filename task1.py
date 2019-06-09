27f57933159137ca5c90db0c145199f8c038247c
nori266 <noriel266@gmail.com>
2019-06-09 15:59:01 +0700

    added running scripts for task 1 and 2

---
+++ task1.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+import sys
+
+from langid.language_identification.fasttext_lang_id import FasttextLangId
+
+if __name__ == '__main__':
+
+    if len(sys.argv) < 2:
+        test_file = input("Please, enter input file name and output file name:")
+    else:
+        test_file = sys.argv[1]
+
+    if len(sys.argv) < 3:
+        output_file = 'langid_output.txt'
+        print(f'Output was saved to {output_file}')
+    else:
+        output_file = sys.argv[2]
+
+
+    model = FasttextLangId('langid/models/lid.176.ftz')
+    output = model.predict_file(test_file)
+
+    with Path(output_file).open('w') as fout:
+        for line in output:
+            fout.write(f'{line}\n')
